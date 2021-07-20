3c0b67dd1f6610ad96cb99ef437a0576dcac7a5d
maksym.valyn <mvalyn@gmail.com>
2021-07-20 15:21:58 +0300

    model modified

--- shop/adjustment/models.py
+++ shop/adjustment/models.py
@@ -8,7 +8,5 @@ class Adjustment(models.Model):
     procurement_price_upper_limit = models.DecimalField(
         max_digits=7, decimal_places=1, default=1.0
     )
-    extra_charge = models.DecimalField(
-        max_digits=4, decimal_places=1
-    )
+    extra_charge = models.DecimalField(max_digits=4, decimal_places=1)
     risk = models.DecimalField(max_digits=3, decimal_places=1)
